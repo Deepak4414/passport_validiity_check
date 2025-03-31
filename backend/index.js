4be196f684e24e7024319c4f70ce52b690c83826
Deepak4414 <dk4414@gmail.com>
2025-03-31 11:51:50 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,13 @@ app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 // Allow requests from your frontend
 app.use(cors({ origin: "https://passport-validiity-check.vercel.app" }));
+
+
+app.get("/api/students", (req, res) => {
+  res.json({ message: "CORS is working!" });
+});
+
+
 // Correct Route Usage
 app.use("/api/students", addStudent);
 
