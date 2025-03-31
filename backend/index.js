7ed98439a562069a86ea7a1f31c47e88d7149c12
Deepak4414 <dk4414@gmail.com>
2025-03-31 19:55:26 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -27,17 +27,17 @@ app.use("/api/students", addStudent);
 app.get("/", (req, res) => {
   res.send("Hello World! Backend is working.");
 });
-const Student = require("./models/Student-Schema");
-app.get("/deepak", async (req, res) => {
-  try {
-    console.log("Fetching students...");
-    const students = await Student.find();
-    return res.status(200).json(students);
-  } catch (error) {
-    console.error("Error fetching students:", error);
-    res.status(500).json({ error: "Failed to fetch students" });
-  }
-});
+// const Student = require("./models/Student-Schema");
+// app.get("/deepak", async (req, res) => {
+//   try {
+//     console.log("Fetching students...");
+//     const students = await Student.find();
+//     return res.status(200).json(students);
+//   } catch (error) {
+//     console.error("Error fetching students:", error);
+//     res.status(500).json({ error: "Failed to fetch students" });
+//   }
+// });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 module.exports = app;
