7d333d892c52b0188ae4aa2a154f83eb81fcb407
Deepak4414 <dk4414@gmail.com>
2025-04-05 09:20:23 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 // ✅ Set CORS Headers
 const corsOptions = {
   origin: "*",
-  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
   allowedHeaders: "Content-Type, Authorization",
   credentials: true,
 };
